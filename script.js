395b4ca2455a33f02f2ce5cbd4e80648756881f4
solveit-homework-help <solveithwhelp@gmail.com>
2024-12-31 12:55:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
